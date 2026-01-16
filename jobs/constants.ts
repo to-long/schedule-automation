833504b3a6029350f7b74637453c2b4e524643fa
to-long <longth.bka@gmail.com>
2026-01-16 11:42:18 +0700

    update user

--- jobs/constants.ts
+++ jobs/constants.ts
@@ -7,6 +7,12 @@ export const USERS: User[] = [
     lat: 20.97148857955816,
     lng: 105.85263257121294,
   },
+  {
+    name: 'Nam',
+    token: '94749|dv9QdIdChx2JFsKfCbMT26MBWZdRh3FkMuG6NyKf',
+    lat: 105.8312632165888,
+    lng: 21.02260493677713,
+  }
 ];
 
 export const API_URL = 'https://d14znnyip8zkld.cloudfront.net/api/check_in_out/reg';
