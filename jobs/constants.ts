a1628c5f7992b8c4b9ec4f8e92dfddda117294e9
to-long <longth.bka@gmail.com>
2026-05-20 08:07:16 +0700

    Update constants.ts

--- jobs/constants.ts
+++ jobs/constants.ts
@@ -3,7 +3,7 @@ import type { User } from './types.js';
 export const USERS: User[] = [
   {
     name: 'Long',
-    token: '110467|PMAduZANcW74yZ3HuStvNMUPAWqzf1b3Bfn5d7tx',
+    token: '22035|ZgrGMqRWjRxA6xSMdhiGmZYrSOUgHVxFsUUVWE8s',
     lat: 20.97148857955816,
     lng: 105.85263257121294,
   },
